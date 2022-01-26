7d6d8c263269fe4ee088f8343d12d8993b14f939
Ocheretny Artem <ocheretnyartem@gmail.ru>
2022-01-26 12:13:30 +0300

    Init

---
+++ app/src/main/java/by/ocheretny/swagger_try/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.ocheretny.swagger_try
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
